eaf071b538cd80f055eabf54c4075c06995d3e5e
Alejandro Herce <alejandro@herce.me>
2016-09-09 07:05:42 -0500

    Lalalala

---
+++ examen_parcial1/examen_parcial1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  examen_parcial1
+//
+//  Created by Alejandro Herce on 9/9/16.
+//  Copyright © 2016 Alejandro Herce. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
