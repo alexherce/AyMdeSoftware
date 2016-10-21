17a76c79b045094145cbeb074dd569f7eb080722
Alejandro Herce <alejandro@herce.me>
2016-10-21 00:08:17 -0500

    Segundo examen parcial creado

---
+++ Proxy/Proxy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Proxy
+//
+//  Created by Alejandro Herce on 9/23/16.
+//  Copyright © 2016 Alejandro Herce. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ examen_parcial2/Ejercicio1/Ejercicio1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ejercicio1
+//
+//  Created by Alejandro Herce on 10/21/16.
+//  Copyright © 2016 Alejandro Herce. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
